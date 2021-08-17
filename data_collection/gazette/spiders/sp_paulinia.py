71d815c4ba4c88e23aa4ac5a9ca27e0d170b1b98
belisards <adrianobf@gmail.com>
2021-08-17 13:26:05 -0300

    update scraper

--- data_collection/gazette/spiders/sp_paulinia.py
+++ data_collection/gazette/spiders/sp_paulinia.py
@@ -3,20 +3,32 @@
 
 import datetime
 
+# import scrapy
+from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
 
 
 class SpPauliniaSpider(BaseGazetteSpider):
     name = "sp_paulinia"
     TERRITORY_ID = "2905206"
+    start_date = datetime.date(2010, 1, 4)
     allowed_domains = ["www.paulinia.sp.gov.br/"]
     start_urls = ["http://www.paulinia.sp.gov.br/semanarios/"]
-    start_date = datetime.date(2020, 4, 3)
 
     def parse(self, response):
-        print(response.url)
+        editions = response.xpath(
+            "//div[@class='container body-content']//div[@class='row']//a[contains(@href, 'AbreSemanario')]"
+        )
+        for edition in editions:
+            final_url = edition.xpath("./@href").get()
+            link = "http://www.paulinia.sp.gov.br/semanarios/" + final_url
+            # description = edition.xpath(
+            #     "./text()"
+            # ).get()
+            # isso retorna string como "29/01/2021 - 1582 - Edição Normal" - falta ainda separar cada info
 
-
-# a_selectors = response.xpath("//a")
-# response.xpath("//div[@class='container body-content']//div[@class='row']//a[contains(@href, 'AbreSemanario')]/@href")
-# response.xpath("//div[@class='container body-content']//div[@class='row']//a[contains(@href, 'AbreSemanario')]/text()")
+            yield Gazette(
+                # edition_number = ,
+                file_urls=[link],
+                power="executive",
+            )
